77798e5dbcee0c5fdba811e6d9d5ec03e5a05a87
Malek Abasise <abdmalek.abassisse@esprit.tn>
2024-07-11 05:43:29 +0100

    tasks (18) filters fix and stubhub fix(5)

--- components/header/header-9/top-header/ContactInfo.jsx
+++ components/header/header-9/top-header/ContactInfo.jsx
@@ -5,7 +5,7 @@ const ContactInfo = () => {
   return (
     <div className="row x-gap-15 y-gap-15 items-center justify-center">
       <div className="col-auto text-white text-14 text-center">
-        We're upfront about it: This website features affiliate links.<span className="md:d-none">Clicking and purchasing through these links supports our efforts.</span> &nbsp;  
+        We're upfront about it: This website features affiliate links.<span className="md:d-none"> Clicking and purchasing through these links supports our efforts.</span>&nbsp; 
         <Link
           href={`/others-pages/terms`}
           className="text-14 text-white text-decoration-underline"
