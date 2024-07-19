2aac48a9e8a5912093b98cff4385a60f9b458fc6
Malek Abasise <abdmalek.abassisse@esprit.tn>
2024-07-19 01:47:35 +0100

    tasks (18) fix topheader paragraph

--- components/header/header-9/top-header/ContactInfo.jsx
+++ components/header/header-9/top-header/ContactInfo.jsx
@@ -4,8 +4,8 @@ import React from "react";
 const ContactInfo = () => {
   return (
     <div className="row x-gap-15 y-gap-15 items-center justify-center">
-      <div className="col-auto text-white text-14 text-center">
-        We're upfront about it: This website features affiliate links.<span className="md:d-none"> Clicking and purchasing through these links supports our efforts.</span>&nbsp; 
+      <div className="col-auto text-white text-13 text-center">
+      <span className="md:d-none">We're upfront about it:</span> This website features affiliate links.<span className=""> Clicking and purchasing through these links supports our efforts.</span>&nbsp; 
         <Link
           href={`/others-pages/terms`}
           className="text-14 text-white text-decoration-underline"
